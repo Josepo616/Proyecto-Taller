132a28fe51b79332454aba322d8750b176fee9ea
José <josemejia61976751@gmail.com>
2024-02-19 07:27:19 -0600

    primer commit

---
+++ Taller-WF/Taller-WF/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Taller_WF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
